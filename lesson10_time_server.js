6e0b2e3f4382661a9592c48bb2f3bd5d7943106a
Siva Sankar <ctsiva25@gmail.com>
2015-10-29 01:44:03 +0530

    Just did pretty print

--- lesson10_time_server.js
+++ lesson10_time_server.js
@@ -1,16 +1,17 @@
 var net = require('net');
 
 function fillZero(number) {
-	return (number < 10 ? '0' + number : number);
+    return (number < 10 ? '0' + number : number);
 }
+
 function getTime() {
-	var date = new Date();	
-	var message = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + fillZero(date.getDate()) + ' ' + fillZero(date.getHours()) + ':' + fillZero(date.getMinutes());
-	return message;
+    var date = new Date();
+    var message = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + fillZero(date.getDate()) + ' ' + fillZero(date.getHours()) + ':' + fillZero(date.getMinutes());
+    return message;
 }
-var server = net.createServer(function (socket) {
-	
-	socket.end(getTime() + '\n');
+var server = net.createServer(function(socket) {
+
+    socket.end(getTime() + '\n');
 
 });
 server.listen(Number(process.argv[2]));
